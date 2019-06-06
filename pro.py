f415b7399c7b7398bf98de14589c7e8985a39f80
Aditya Abhiram <aa20475@gmail.com>
2019-06-06 17:16:53 +0530

    My Changes!
    Segmentation Attempts and PDFs for reference.

--- pro.py
+++ pro.py
@@ -4,10 +4,52 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 import wfdb
+import math
+
+#Kernel function for Segmentation
+def kernel(X,t):
+	if t==0:
+		print("Error: t can't be zero")
+	else:
+		g=[]
+
+		for x in X:
+			g.append(math.exp((x*x)/(2*t))/(math.sqrt(2*np.pi*t)))
+
+		return g
+
+#convolution function for finite signals
+def convolution(f,g,M,x):
+	p=0
+	for n in range(-int(M),int(M)+1):
+		if x-n>0 and x-n<4000:
+			p+= f[x-n]*g[n]
+		else:
+			p+=0
+
+	return p
+
+def lin_interpolate(x1,y1,x2,y2,d,t):
+	m=[]
+	if t==True:
+		t_x = np.arange(x1,x2,(x2-x1)/d)
+		for x in t_x:
+			m.append((((y2-y1)*(x-x1))/(x2-x1)) + y1)
+		return m , t_x
+	else:
+		t_x = np.arange(x1+(x2-x1)/(d-1),x2,(x2-x1)/(d-1))
+		for x in t_x:
+			m.append((((y2-y1)*(x-x1))/(x2-x1)) + y1)
+		return m , t_x
+
 
 #importing data from a patients record
-signals , fields = wfdb.rdsamp("s0010_re",sampto=3999)
+signals , fields = wfdb.rdsamp("s0010_re",sampto=4000)
 
+
+freq = np.arange(0,np.pi,np.pi/4000)
+
+print(freq)
 
 #separated v1 for testing
 v1 = []
@@ -16,9 +58,9 @@ for x in range(len(signals)):
 	v1.append(signals[x][6])
 
 
-plt.plot(v1)
-plt.show()
-
+#plt.plot(v1)
+#plt.show()
+print("Signal appended")
 
 fc = 0.5  # Cut-off frequency of the filter
 fs = 1000
@@ -26,8 +68,7 @@ w = fc / (fs / 2) # Normalizing the frequency
 b, a = signal.butter(5, w, 'high')
 output = signal.filtfilt(b, a, v1) #High pass filtering with the given cut-off frequency
 
-plt.plot(output)
-plt.show()
+print("filtered")
 
 #FBSE application
 fbse = []
@@ -40,12 +81,22 @@ for x in range(N):
 	fbse.append(2*x*np.pi*k/N)
 fbse1 = abs(np.array(fbse))
 
+print("Transformed")
 
-#inversing the FBSE
-infbse = np.zeros(N)
-for x in range(1,N):
-	for i in range(1,N):
-		infbse[x] = infbse[x] + fbse[i-1]*np.sin(i*np.pi*x/N)*N/(i*np.pi*N)
-plt.plot(infbse)
+t = []
 
-plt.show()
+nstep = 6
+
+for x in range(1,nstep+1):
+	t.append(x*0.5)
+
+
+L = []
+
+for x in range(len(freq)):
+	c=[]
+	for p in t:
+		c.append(convolution(fbse1,kernel(freq,p),6*p+1,x))
+	L.append(c)
+
+
